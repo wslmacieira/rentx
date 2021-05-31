8620e59ac151814e101624da5c55cb24636c7ba1
wslmacieira <wslmacieira@outlook.com>
2021-05-31 13:06:15 -0300

    Conhecendo Singleton Pattern

--- src/modules/car/useCases/createCategory/index.ts
+++ src/modules/car/useCases/createCategory/index.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 import { CreateCategoryController } from "./CreateCategoryController";
 import { CreateCategoryUsecase } from "./CreateCategoryUseCase";
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getInstance();
 
 const createCategoryUseCase = new CreateCategoryUsecase(categoriesRepository);
 
