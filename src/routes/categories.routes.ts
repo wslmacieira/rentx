a142a6aab5f2c99b47daac4be34648fb00f5f34d
wslmacieira <wslmacieira@outlook.com>
2021-05-31 08:11:09 -0300

    Criando service de especificação e separando em módulos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { CategoriesRepository } from "../modules/car/repositories/CategoriesRepository";
+import { CreateCategoryService } from "../modules/car/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
