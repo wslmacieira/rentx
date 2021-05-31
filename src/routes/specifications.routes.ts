9e1345cfeb9a7201a303962d90694b21f1906404
wslmacieira <wslmacieira@outlook.com>
2021-05-31 09:03:55 -0300

    Criando repositório de especificação

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+
+import { SpecificationsRepository } from "../modules/car/repositories/SpecificationsRepository";
+import { CreateSpecificationService } from "../modules/car/services/CreateSpecificationService";
+
+const specificationsRoutes = Router();
+
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+  const createCategoryService = new CreateSpecificationService(
+    specificationsRepository
+  );
+
+  createCategoryService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+export { specificationsRoutes };
