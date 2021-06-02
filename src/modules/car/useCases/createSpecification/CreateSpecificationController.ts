efea7ddf4757ce8d99028ec7afbc06801511b5ed
wslmacieira <wslmacieira@outlook.com>
2021-06-02 20:44:37 -0300

    Refatorando as especificações

--- src/modules/car/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/car/useCases/createSpecification/CreateSpecificationController.ts
@@ -1,13 +1,17 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { CreateSpecificationUsecase } from "./CreateSpecificationUseCase";
 
 class CreateSpecificationController {
-  constructor(private createSpecificationUsecase: CreateSpecificationUsecase) {}
   handle(request: Request, response: Response): Response {
     const { name, description } = request.body;
 
-    this.createSpecificationUsecase.execute({ name, description });
+    const createSpecificationUsecase = container.resolve(
+      CreateSpecificationUsecase
+    );
+
+    createSpecificationUsecase.execute({ name, description });
 
     return response.status(201).send();
   }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,8 +2,8 @@ import { Router } from "express";
 import multer from "multer";
 
 import { CreateCategoryController } from "../modules/car/useCases/createCategory/CreateCategoryController";
-import { importCategoryController } from "../modules/car/useCases/importCategory";
-import { listCategoriesController } from "../modules/car/useCases/listCategories";
+import { ImportCategoryController } from "../modules/car/useCases/importCategory/ImportCategoryController";
+import { ListCategoriesController } from "../modules/car/useCases/listCategories/ListCategoriesController";
 
 const categoriesRoutes = Router();
 
@@ -12,15 +12,17 @@ const upload = multer({
 });
 
 const createCategoryController = new CreateCategoryController();
+const importCategoryController = new ImportCategoryController();
+const listCategoriesController = new ListCategoriesController();
 
 categoriesRoutes.post("/", createCategoryController.handle);
 
-categoriesRoutes.get("/", (request, response) => {
-  return listCategoriesController.handle(request, response);
-});
+categoriesRoutes.get("/", listCategoriesController.handle);
 
-categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-  return importCategoryController.handle(request, response);
-});
+categoriesRoutes.post(
+  "/import",
+  upload.single("file"),
+  importCategoryController.handle
+);
 
 export { categoriesRoutes };
