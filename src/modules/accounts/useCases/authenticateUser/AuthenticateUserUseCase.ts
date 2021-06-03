49e78ee220e0a6ea81ffab240316d73e9b2d326e
wslmacieira <wslmacieira@outlook.com>
2021-06-03 13:25:41 -0300

    Tratamento de excessões

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -28,14 +29,14 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error("Email or password incorrect!");
+      throw new AppError("Email or password incorrect!");
     }
 
     // Verificar se a senha está correta
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error("Email or password incorrect!");
+      throw new AppError("Email or password incorrect!");
     }
     // Gerar o jsonwebtoken
     const token = sign({}, "bef27466a245ce3ec692bd25409c2549", {
