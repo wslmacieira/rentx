e01fa181c3d76dbafada6b3c51b02145d7e47d0a
wslmacieira <wslmacieira@outlook.com>
2021-06-03 14:11:34 -0300

    Adicionando coluna de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,24 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { CreateUserController } from "../modules/accounts/useCases/CreateUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
 
+const upload = multer({
+  dest: "./avatar",
+});
+
 const createUsersController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUsersController.handle);
+
+usersRoutes.patch(
+  "/avatar",
+  upload.single("file"),
+  updateUserAvatarController.handle
+);
 
 export { usersRoutes };
