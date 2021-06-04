f3a1e380d61e696c630af7e6e8891a8a48e39c50
wslmacieira <wslmacieira@outlook.com>
2021-06-04 20:19:48 -0300

    Teste de autenticação do usuário

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,63 @@
+import { AppError } from "../../../../errors/AppError";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryImemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../CreateUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let usersRepositoryImemory: UsersRepositoryImemory;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    usersRepositoryImemory = new UsersRepositoryImemory();
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      usersRepositoryImemory
+    );
+    createUserUseCase = new CreateUserUseCase(usersRepositoryImemory);
+  });
+
+  it("shoud be able to authenticate an user", async () => {
+    const user: ICreateUserDTO = {
+      driver_license: "000123",
+      email: "user@test.com",
+      password: "1234",
+      name: "User Test",
+    };
+    await createUserUseCase.execute(user);
+
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(result).toHaveProperty("token");
+  });
+
+  it("shoud not be able to authenticate an nonexistent user", async () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: "false@email.com",
+        password: "1234",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to authenticate with incorrect password", () => {
+    expect(async () => {
+      const user: ICreateUserDTO = {
+        driver_license: "9999",
+        email: "user@user.com",
+        password: "1234",
+        name: "User Test Error",
+      };
+
+      await createUserUseCase.execute(user);
+
+      await authenticateUserUseCase.execute({
+        email: user.email,
+        password: "incorrectPassword",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
--- src/modules/car/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/car/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -24,8 +24,6 @@ describe("Create Category", () => {
       category.name
     );
 
-    console.log(categoyCreated);
-
     expect(categoyCreated).toHaveProperty("id");
   });
 
