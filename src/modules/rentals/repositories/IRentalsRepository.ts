c3653a9c360db86ae9c46a053d15d7897e94dbef
wslmacieira <wslmacieira@outlook.com>
2021-10-16 22:09:57 -0300

    Cria cadastro de aluguel

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -1,8 +1,10 @@
+import { ICreateRentalDTO } from "../infra/typeorm/dto/ICreateRentalDTO";
 import { Rental } from "../infra/typeorm/entities/Rental";
 
 interface IRentalsRepository {
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
+  create(data: ICreateRentalDTO): Promise<Rental>;
 }
 
 export { IRentalsRepository };
